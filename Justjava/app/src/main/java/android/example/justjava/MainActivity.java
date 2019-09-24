6bded07ab3ac94ef3ed057dfaad320098b631963
Vlad Stashevskiy <vladisoriginals@icloud.com>
2019-09-25 00:17:18 +0300

    Homework: Movie trailer. Constraint layout

--- Justjava/app/src/main/java/android/example/justjava/MainActivity.java
+++ Justjava/app/src/main/java/android/example/justjava/MainActivity.java
@@ -6,49 +6,40 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.text.NumberFormat;
-
 public class MainActivity extends AppCompatActivity {
-    int numberOfCoffees = 0;
+    int quantity = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    /**
-     * This method displays the given quantity value on the screen.
-     */
-    public void display(int number) {
+
+    public void displayQuantity(int number) {
         TextView quantityTextView = findViewById(R.id.tv_quantity);
             quantityTextView.setText("" + number);
 
     }
-    /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(String number) {
-        TextView priceTextView = findViewById(R.id.tv_price);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    private int calculatePrice(int quantity){
+        int price = quantity * 5;
+        return price;
     }
     private void displayMessage(String message){
         TextView priceTextView = findViewById(R.id.tv_price);
         priceTextView.setText(message);
     }
-    /**
-     * This method is called when the order button is clicked.
-     */
+
     public void submitOrder(View view) {
-        String priceMessage = "Total: "+numberOfCoffees*5+"$\n";
+        String priceMessage = "Total: "+calculatePrice(quantity)+"$\n";
         String thank = "Thank you!";
         displayMessage(priceMessage+thank);
     }
     public void increment(View view){
-        numberOfCoffees=numberOfCoffees+1;
-        display(numberOfCoffees);
+        quantity=quantity+1;
+        displayQuantity(quantity);
     }
     public void decrement(View view){
-        numberOfCoffees=numberOfCoffees-1;
-        display(numberOfCoffees<=0?numberOfCoffees=0:numberOfCoffees);
+        quantity=quantity-1;
+        displayQuantity(quantity<=0?quantity=0:quantity);
 
        }
 }
