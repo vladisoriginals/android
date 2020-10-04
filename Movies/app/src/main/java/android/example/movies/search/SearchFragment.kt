340c3c11998f9c15c34601a884be357719d80812
Oprajes <ostrovskiy1199@gmail.com>
2020-10-04 18:18:38 +0300

    refactor SearchFragment

--- Movies/app/src/main/java/android/example/movies/search/SearchFragment.kt
+++ Movies/app/src/main/java/android/example/movies/search/SearchFragment.kt
@@ -1,15 +1,16 @@
 package android.example.movies.search
 
+import android.example.movies.databinding.FragmentSearchBinding
+import android.example.movies.domain.Movie
+import android.example.movies.utils.AdapterMovies
+import android.example.movies.utils.MoviesListener
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.example.movies.databinding.FragmentSearchBinding
-import android.example.movies.utils.AdapterMovies
-import android.example.movies.utils.MoviesListener
 import android.widget.Toast
-import androidx.lifecycle.ViewModelProviders
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import kotlinx.coroutines.flow.collect
@@ -17,47 +18,32 @@ import kotlinx.coroutines.launch
 
 class SearchFragment : Fragment() {
 
-    private lateinit var adapter: AdapterMovies
-    private lateinit var binding: FragmentSearchBinding
-
-    private val viewModel: SearchViewModel by lazy {
-        val activity = requireNotNull(this.activity) {
-            "You can only access the viewModel after onActivityCreated()"
-        }
-        ViewModelProviders.of(this, SearchViewModel.Factory(activity.application))
-            .get(SearchViewModel::class.java)
+    private val adapter = AdapterMovies(MoviesListener(::onItemClick))
+    private val viewModel: SearchViewModel by viewModels {
+        SearchViewModel.Factory(requireActivity().application)
     }
 
-
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
-        binding = FragmentSearchBinding.inflate(inflater)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding = FragmentSearchBinding.inflate(inflater)
         binding.lifecycleOwner = viewLifecycleOwner
         binding.viewModel = viewModel
-
-        initMoviesList()
-
-        viewLifecycleOwner.lifecycleScope.launch {
-            viewModel.error.collect { showNetworkError() }
-        }
-
+        binding.listMovies.adapter = adapter
         return binding.root
     }
 
-    private fun initMoviesList(){
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.error.collect { showNetworkError() }
+        }
+    }
 
-        adapter = AdapterMovies(MoviesListener { movies ->
-            val navController = findNavController()
-            navController.navigate(SearchFragmentDirections.actionSearchFragmentToDetailsFragment(movies))
-        })
-        binding.listMovies.adapter = adapter
+    private fun onItemClick(movie: Movie) {
+        findNavController().navigate(SearchFragmentDirections.toDetailsFragment(movie))
     }
 
     private fun showNetworkError() {
         Toast.makeText(activity, "NetworkError", Toast.LENGTH_LONG).show()
     }
+
 }
