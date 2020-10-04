a68d6aaa8661aaf42b3a25d0dff5775504b09dcd
Oprajes <ostrovskiy1199@gmail.com>
2020-10-04 18:21:40 +0300

    refactor DetailsViewModel

--- Movies/app/src/main/java/android/example/movies/details/DetailsViewModel.kt
+++ Movies/app/src/main/java/android/example/movies/details/DetailsViewModel.kt
@@ -5,48 +5,27 @@ import android.example.movies.database.MoviesDatabase
 import android.example.movies.domain.Movie
 import android.example.movies.domain.Video
 import android.example.movies.repository.MoviesRepository
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
+import androidx.lifecycle.*
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class DetailsViewModel(
     private val movie: Movie,
-    private val app: Application
+    app: Application
 ) : ViewModel() {
 
     private val repository = MoviesRepository(MoviesDatabase.getInstance(app))
-    private val viewModelJob = SupervisorJob()
-    private val viewModelScope = CoroutineScope(viewModelJob + Dispatchers.Main)
-
     private val _trailer = MutableLiveData<Video>()
     val trailer: LiveData<Video> = _trailer
-
-
-    private fun getUrl() {
-        viewModelScope.launch {
-            repository.getTrailerFromNetwork(movie)
-
-        }
-    }
 
     init {
         viewModelScope.launch {
             repository.fetchTrailer(movie.id).collect { _trailer.value = it }
         }
-        getUrl()
+        viewModelScope.launch {
+            repository.getTrailerFromNetwork(movie)
+        }
     }
-
-    override fun onCleared() {
-        super.onCleared()
-        viewModelJob.cancel()
-    }
-
 
     class Factory(val movie: Movie, val app: Application) : ViewModelProvider.Factory {
         override fun <T : ViewModel?> create(modelClass: Class<T>): T {
